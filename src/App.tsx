885211f6eacfd16089716f0d8eadf5bee3aec3d0
italoCesarSilvaOliveira <italo.cs.oliveira@gmail.com>
2024-04-08 13:56:35 -0300

    Add divs in APP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import './App.css'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='container'>
+      <div className='sidebar-info'></div>
+      <div className='main-content'>
+        <div className='nav-bar'></div>
+        <div className="about"></div>
+        <div className="resume"></div>
+        <div className="portfolio"></div>
+        <div className="skills"></div>
+        <div className="qualities"></div>
+      </div>
+    </div>
   )
 }
 
