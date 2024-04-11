9d885211bc500f7b258695642bf1b9f4f397b4cd
italoCesarSilvaOliveira <italo.cs.oliveira@gmail.com>
2024-04-11 13:40:01 -0300

    The theme  folder was changed

---
+++ src/theme/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primaryBackground: '#383838',
+  secondaryBackground: '#2b2b2c',
+  tertiaryBackground: '#2b2b2cbf',
+  background: '#202022',
+  accentBackground: '#1e1e1f',
+  darkBackground: '#121212',
+  darkestBackground: '#0d0d0d',
+  border: '#383838',
+  lightGray: '#fafafa',
+  grayTransparent: '#d6d6d6b3',
+  gold: '#ffdb70',
+  white: '#FFFFFF',
+  gray: '#d6d6d6',
+  purple: '#551a8b',
+  black: '#000000'
+};
