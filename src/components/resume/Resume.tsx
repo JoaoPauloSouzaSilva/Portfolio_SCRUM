2a3fa4657a48984a202849af140cfd5c697d31b6
italoCesarSilvaOliveira <italo.cs.oliveira@gmail.com>
2024-04-16 15:03:41 -0300

    Finalizing summary screen pattern

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -1,12 +1,33 @@
-import './Resume_Styles.css';
+import "./Resume_Styles.css";
 
-function Resume(){
+export default function Resume() {
   return (
-  
-      <div className='Resume-container'>
-       
+    <div className="Resume-container">
+      <div className="resume-header">
+        <h2 className="resume-title">Habilidades</h2>
+        <div className="resume-line"></div>
       </div>
-  )
+      <section className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box"></div>
+          <h3 className="h3"></h3>
+        </div>
+        <ol className="timeline-list">
+          <li className="timeline-item">
+            <h4 className="h4 timelime-item-title"></h4>
+            <p className="timeline-text"></p>
+          </li>
+          <li className="timeline-item">
+            <h4 className="h4 timelime-item-title"></h4>
+            <p className="timeline-text"></p>
+          </li>
+          <li className="timeline-item">
+            <h4 className="h4 timelime-item-title"></h4>
+            <p className="timeline-text"></p>
+          </li>
+        </ol>
+      </section>
+      <section className="timeline"></section>
+    </div>
+  );
 }
-
-export default Resume
