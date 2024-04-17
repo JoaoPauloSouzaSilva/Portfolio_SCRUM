5ab8b440343970f01f394af1530c4a1afc676800
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-04-17 14:46:00 -0300

    Editing About Screen And transform Activities in Components

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,39 +1,17 @@
 import { useState } from 'react';
 import './About_Styles.css';
-import * as Unicons from '@iconscout/react-unicons';
 import ModalActivities from './Modal_Activities';
 import Carousel from './About_Carousel_Testimonials';
 import Title from '../Title_Screens/Title';
+import Activities from './About_Doing_Activities';
 
 
 function About(){ 
   const [isOpen_Activite01, setIsOpen_Activite01] = useState(false);
-  const [isOpen_Activite02, setIsOpen_Activite02] = useState(false)
-  const [isOpen_Activite03, setIsOpen_Activite03] = useState(false)
-  const [isOpen_Activite04, setIsOpen_Activite04] = useState(false)
+  const [isOpen_Activite02, setIsOpen_Activite02] = useState(false);
+  const [isOpen_Activite03, setIsOpen_Activite03] = useState(false);
+  const [isOpen_Activite04, setIsOpen_Activite04] = useState(false);
 
-  const handleOpen_Activite01Modal = () => {
-    setIsOpen_Activite01(true);
-  };
-
-  const handleOpen_Activite02Modal = () => {
-    setIsOpen_Activite02(true);
-  };
-
-  const handleOpen_Activite03Modal = () => {
-    setIsOpen_Activite03(true);
-  };
-
-  const handleOpen_Activite04Modal = () => {
-    setIsOpen_Activite04(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsOpen_Activite01(false);
-    setIsOpen_Activite02(false);
-    setIsOpen_Activite03(false);
-    setIsOpen_Activite04(false);
-  };
   return (
   
       <div className='About-container'>
@@ -56,74 +34,67 @@ function About(){
           <h3 className="About-doing-header">O Que Eu Estou Fazendo</h3>
           <div className="About-doing-container-activities">
 
-            <div className="About-doing-activities" onClick={handleOpen_Activite01Modal}>
-              <div className="activities-icon"><Unicons.UilPen size="40" /></div>
-              <div className="activities-content">
-                <h4 className="activities-title">Designer De Web</h4>
-                <p className="activities-text">O design mais moderno e de alta qualidade feito a nível profissional.</p>
-              </div>
-            </div>
-
-            <ModalActivities  
-              isOpen={isOpen_Activite01}   
-              onClose={handleCloseModal} 
-              title="Título do Modal"   
-              text="Texto do Modal"   
-              icon="UilPen"
+            <Activities 
+              onClick={() => setIsOpen_Activite01(true)}
+              Title={'Designer De Web'} 
+              Text={'O design mais moderno e de alta qualidade feito a nível profissional.'} 
+              NameIcon={'UilPen'}
             />
 
-            <div className="About-doing-activities" onClick={handleOpen_Activite02Modal}>
-              <div className="activities-icon"><Unicons.UilBrowser size="40"/></div>
-              <div className="activities-content">
-                <h4 className="activities-title">Desenvolvimento Web</h4>
-                <p className="activities-text">Desenvolvimento de sites de alta qualidade a nível profissional.</p>
-              </div>
-            </div>
+              <ModalActivities  
+                isOpen={isOpen_Activite01}   
+                onClose={() => setIsOpen_Activite01(false)} 
+                title="Designer De Web"   
+                text="O design mais moderno e de alta qualidade feito a nível profissional."   
+                icon="UilPen"
+              />
 
-            <ModalActivities  
-              isOpen={isOpen_Activite02}   
-              onClose={handleCloseModal} 
-              title="Título do Modal"   
-              text="Texto do Modal"   
-              icon="UilBrowser"
+            <Activities 
+              onClick={() => setIsOpen_Activite02(true)}
+              Title={'Desenvolvimento Web'} 
+              Text={'O design mais moderno e de alta qualidade feito a nível profissional.'} 
+              NameIcon={'UilBrowser'}
             />
+
+              <ModalActivities  
+                isOpen={isOpen_Activite02}   
+                onClose={() => setIsOpen_Activite02(false)} 
+                title="Desenvolvimento Web"   
+                text="O design mais moderno e de alta qualidade feito a nível profissional."   
+                icon="UilBrowser"
+              />
+
+            <Activities 
+              onClick={() => setIsOpen_Activite03(true)}
+              Title={'Fotografia'} 
+              Text={'>Faço fotos de alta qualidade de qualquer categoria a nível profissional.'} 
+              NameIcon={'UilCamera'}
+            />
+
+              <ModalActivities  
+                isOpen={isOpen_Activite03}   
+                onClose={() => setIsOpen_Activite03(false)} 
+                title="Fotografia"   
+                text=">Faço fotos de alta qualidade de qualquer categoria a nível profissional."   
+                icon="UilCamera"
+              />
+
+            <Activities 
+              onClick={() => setIsOpen_Activite04(true)}
+              Title={'Aplicativos Móveis'} 
+              Text={'Desenvolvimento profissional de aplicativos para iOS e Android.'} 
+              NameIcon={'UilMobileAndroid'}
+            />
+
+              <ModalActivities  
+                isOpen={isOpen_Activite04}   
+                onClose={() => setIsOpen_Activite04(false)} 
+                title="Aplicativos Móveis"   
+                text="Desenvolvimento profissional de aplicativos para iOS e Android."   
+                icon="UilMobileAndroid"
+              />
 
           </div>
-          <div className="About-doing-container-activities">
-
-            <div className="About-doing-activities" onClick={handleOpen_Activite03Modal}>
-              <div className="activities-icon"><Unicons.UilMobileAndroid size="40" /></div>
-              <div className="activities-content">
-                <h4 className="activities-title">Aplicativos Móveis</h4>
-                <p className="activities-text">Desenvolvimento profissional de aplicativos para iOS e Android.</p>
-              </div>
-            </div>
-
-            <ModalActivities  
-              isOpen={isOpen_Activite03}   
-              onClose={handleCloseModal} 
-              title="Título do Modal"   
-              text="Texto do Modal"   
-              icon="UilMobileAndroid"
-            />
-
-            <div className="About-doing-activities" onClick={handleOpen_Activite04Modal}>
-              <div className="activities-icon"><Unicons.UilCamera size="40" /></div>
-              <div className="activities-content">
-                <h4 className="activities-title">Fotografia</h4>
-                <p className="activities-text">Faço fotos de alta qualidade de qualquer categoria a nível profissional.</p>
-              </div>
-            </div>
-
-            <ModalActivities  
-              isOpen={isOpen_Activite04}   
-              onClose={handleCloseModal} 
-              title="Título do Modal"   
-              text="Texto do Modal"   
-              icon="UilCamera"
-            />
-
-            </div>
         </div>
 
         <div className="About-testimonials">
---
+++ src/components/about/About_Doing_Activities.tsx
@@ -0,0 +1,29 @@
+import './About_Doing_Activities_Styles.css';
+import * as Unicons from '@iconscout/react-unicons';
+
+
+interface ActivitiesProps {
+  Title: string;
+  Text: string; 
+  NameIcon: string;
+  onClick?: () => void;
+}
+
+
+const Activities: React.FC<ActivitiesProps> = ({Title, Text, NameIcon, onClick}) => {
+
+  const Icon = Unicons[NameIcon];
+
+  return (
+    <div className="Activities-container" onClick={onClick ? onClick : undefined}>
+      {/* <div className="About-doing-activities"> */}
+        <div className="Activities-icon"><Icon size="40"/></div>
+        <div className="Activities-content">
+          <h4 className="Activities-title">{Title}</h4>
+          <p className="Activities-text">{Text}</p>
+        </div>
+    </div>
+  );
+}
+
+export default Activities;
