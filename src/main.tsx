354113116136db1ec1185667fbc716821f01a54e
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-04-18 13:58:10 -0300

    Creating function the random colors

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,29 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+const root = document.documentElement as HTMLElement;
+
+const colors = ['#FF7089', '#869cff', 
+                '#FFDB70', '#81fc98', 
+                '#70FFDB','#E6876E' ];
+
+let currentIndex = -1;
+
+function changeColors() {
+  let randomIndex;
+  do {
+      randomIndex = Math.floor(Math.random() * colors.length);
+  } while (randomIndex === currentIndex);
+    currentIndex = randomIndex;
+    const randomColor = colors[randomIndex];
+    root.style.setProperty('--random-color', randomColor);
+
+    //test function
+    // root.style.setProperty('--gold', randomColor);
+
+}
+
+changeColors();
+
+
