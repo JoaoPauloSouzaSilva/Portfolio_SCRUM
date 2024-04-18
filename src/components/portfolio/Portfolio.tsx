45f3dc862690e0ecc2fa1109a384f267937780af
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-04-18 18:32:09 -0300

    Creat Portfolio

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -2,11 +2,23 @@ import './Portfolio_Styles.css';
 import Title from '../Title_Screens/Title';
 
 function Portfolio(){
-  return (
-  
-      <div className='Portfolio-container'>
-        <Title Title={'Portfólio'}/>
+  return ( 
+    <div className='Portfolio-container'>
+      <Title Title={'Portfólio'}/>
+
+      <div className="Portfolio-container-card">
+
+        <div className="Project-Card-Container">
+          <img className="Project-Card-Img" src="src/assets/images/projects/project-1.png" alt="Imagem Projeto" />
+          <div className="Project-Card-Container-Text">
+            <h3 className="Project-Card-Title">Orizon</h3>
+            <p className="Project-Card-Tipe">Web Development</p>
+          </div>
+        </div>
+
+
       </div>
+    </div>
   )
 }
 
