ad2f38f747264f1a78d7377697835b90987da1e5
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-06-11 18:02:27 -0300

    Changes for deployment

--- src/components/portfolio/ProjectData.ts
+++ src/components/portfolio/ProjectData.ts
@@ -1,16 +1,29 @@
+import to_do_list_react from "../../assets/images/projects/to_do_list_react.png";
+import apirest from "../../assets/images/projects/apirest.png";
+import coleiras_de_luz from "../../assets/images/projects/coleiras_de_luz.png";
+import honey_fresh from "../../assets/images/projects/honey_fresh.png";
+import ifstorage from "../../assets/images/projects/ifstorage.png";
+import jogo_da_memoria from "../../assets/images/projects/jogo_da_memoria.png";
+import oscar2023 from "../../assets/images/projects/oscar2023.png";
+import pokedex from "../../assets/images/projects/pokedex.png";
+import poojava from "../../assets/images/projects/poojava.png";
+import portfolio from "../../assets/images/projects/portfolio.png";
+import show_do_milhao from "../../assets/images/projects/show_do_milhao.png";
+import to_do_list_html from "../../assets/images/projects/to_do_list_html.png";
+
 const ProjectData = [
-  { Title: 'To-Do List', Type: 'Desenvolvimento Web', ImgSrc: 'src/assets/images/projects/to_do_list_react.png', Url: 'https://github.com/JoaoPauloSouzaSilva/To-Do-List-Raect-Js'},
-  { Title: 'iFstorage', Type: 'Aplicativo', ImgSrc: 'src/assets/images/projects/ifstorage.png', Url: 'https://github.com/ifnmgsal-inf/Prototipo-iFstorage'},
-  { Title: 'Coleiras de Luz', Type: 'Aplicativo', ImgSrc: 'src/assets/images/projects/coleiras_de_luz.png', Url: 'https://github.com/ifnmgsal-inf/coleiras-de-luz'},
-  { Title: 'Poo Java', Type: 'Back-End', ImgSrc: 'src/assets/images/projects/poojava.png', Url: 'https://replit.com/repls/folder/POO%20Java'},
-  { Title: 'Pokédex', Type: 'Desenvolvimento Web', ImgSrc: 'src/assets/images/projects/pokedex.png', Url: 'https://github.com/JoaoPauloSouzaSilva/pokedex'},
-  { Title: 'Oscar 2023', Type: 'Aplicativo', ImgSrc: 'src/assets/images/projects/oscar2023.png', Url: 'https://github.com/JoaoPauloSouzaSilva/Oscar2023-ReactNative?tab=readme-ov-file'},
-  { Title: 'To-Do List', Type: 'Desenvolvimento Web', ImgSrc: 'src/assets/images/projects/to_do_list_html.png', Url: 'https://github.com/JoaoPauloSouzaSilva/todo_list'},
-  { Title: 'API Rest', Type: 'Back-End', ImgSrc: 'src/assets/images/projects/apirest.png', Url: 'https://github.com/JoaoPauloSouzaSilva/API-REST'},
-  { Title: 'Jogo da Memória', Type: 'Desenvolvimento Web', ImgSrc: 'src/assets/images/projects/jogo_da_memoria.png', Url: 'https://github.com/JoaoPauloSouzaSilva/jogo_da_memoria?tab=readme-ov-file'},
-  { Title: 'Show do Milhão', Type: 'Back-End', ImgSrc: 'src/assets/images/projects/show_do_milhao.png', Url: 'https://github.com/JoaoPauloSouzaSilva/Show-do-milhao'},
-  { Title: 'Honey Fresh', Type: 'Desenvolvimento Web', ImgSrc: 'src/assets/images/projects/honey_fresh.png', Url: 'https://github.com/JoaoPauloSouzaSilva/Bootstrap_Activity'},
-  { Title: 'Portfolio', Type: 'Desenvolvimento Web', ImgSrc: 'src/assets/images/projects/portfolio.png', Url: 'https://github.com/JoaoPauloSouzaSilva/Portfolio_SCRUM'},
+  { Title: 'To-Do List', Type: 'Desenvolvimento Web', ImgSrc: to_do_list_react, Url: 'https://github.com/JoaoPauloSouzaSilva/To-Do-List-Raect-Js'},
+  { Title: 'iFstorage', Type: 'Aplicativo', ImgSrc: ifstorage, Url: 'https://github.com/ifnmgsal-inf/Prototipo-iFstorage'},
+  { Title: 'Coleiras de Luz', Type: 'Aplicativo', ImgSrc: coleiras_de_luz, Url: 'https://github.com/ifnmgsal-inf/coleiras-de-luz'},
+  { Title: 'Poo Java', Type: 'Back-End', ImgSrc: poojava, Url: 'https://replit.com/repls/folder/POO%20Java'},
+  { Title: 'Pokédex', Type: 'Desenvolvimento Web', ImgSrc: pokedex, Url: 'https://github.com/JoaoPauloSouzaSilva/pokedex'},
+  { Title: 'Oscar 2023', Type: 'Aplicativo', ImgSrc: oscar2023, Url: 'https://github.com/JoaoPauloSouzaSilva/Oscar2023-ReactNative?tab=readme-ov-file'},
+  { Title: 'To-Do List', Type: 'Desenvolvimento Web', ImgSrc: to_do_list_html, Url: 'https://github.com/JoaoPauloSouzaSilva/todo_list'},
+  { Title: 'API Rest', Type: 'Back-End', ImgSrc: apirest, Url: 'https://github.com/JoaoPauloSouzaSilva/API-REST'},
+  { Title: 'Jogo da Memória', Type: 'Desenvolvimento Web', ImgSrc: jogo_da_memoria, Url: 'https://github.com/JoaoPauloSouzaSilva/jogo_da_memoria?tab=readme-ov-file'},
+  { Title: 'Show do Milhão', Type: 'Back-End', ImgSrc: show_do_milhao, Url: 'https://github.com/JoaoPauloSouzaSilva/Show-do-milhao'},
+  { Title: 'Honey Fresh', Type: 'Desenvolvimento Web', ImgSrc: honey_fresh, Url: 'https://github.com/JoaoPauloSouzaSilva/Bootstrap_Activity'},
+  { Title: 'Portfolio', Type: 'Desenvolvimento Web', ImgSrc: portfolio, Url: 'https://github.com/JoaoPauloSouzaSilva/Portfolio_SCRUM'},
 ];
 
 export default ProjectData;
